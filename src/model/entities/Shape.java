a4bbabd544153df9986bfc348b58d0ba675f073d
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-09-08 17:15:25 -0300

    Area com Interface

--- src/model/entities/Shape.java
+++ src/model/entities/Shape.java
@@ -1,22 +1,8 @@
 package model.entities;
 
-import model.enums.Color;
 
-public abstract class Shape {
 
-	private Color color;
+public interface Shape {
 	
-	public Shape(Color color) {
-		this.color = color;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	public abstract double area();
+	double area();
 }
